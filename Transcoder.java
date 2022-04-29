8c4008b9a160f88dd672f78ba150d120869ddf08
levertyk <levertyk4517@my.uwstout.edu>
2022-04-29 13:56:57 -0500

    Basic Setup pt2

---
+++ Transcoder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * @author <a href="mailto:keenanleverty@gmail.com">Keenan Leverty</a>, //TODO add authors
+ * 
+ * 
+ */
+
+public class Transcoder {
+
+    /**
+     * Run the program. Prompt the user for the file path of the java file to be trascoded.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
