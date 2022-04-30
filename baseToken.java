7783f24401ad83e06181f1a334ac9ab53ae7ddd9
petermatern <58228250+petermatern@users.noreply.github.com>
2022-04-30 16:20:10 -0500

    Update baseToken.java

--- baseToken.java
+++ baseToken.java
@@ -9,20 +9,54 @@ public class BaseToken extends Token {
 
         if(input.startsWith("while")){
             int tempScope=0;
+            int endOfComplex=0;
             for(int i=input.indexOf("{");i<input.length();i++){
-                if(input.charAt(i).equals('{')){
+                if(input.charAt(i)=='{'){
                     tempScope++;
                 }else if(input.charAt(i)=='}'){
-                    
-                })
+                    tempScope--;
+                }
+                if(tempScope==0){
+                    endOfComplex=i;
+                    break;
+                }
 
             }
-
+            simpOrComp=new ComplexToken(input.substring(0,endOfComplex),scope+1,false);
+            
         }else if(input.startsWith("if")){
+            int tempScope=0;
+            int endOfComplex=0;
+            for(int i=input.indexOf("{");i<input.length();i++){
+                if(input.charAt(i)=='{'){
+                    tempScope++;
+                }else if(input.charAt(i)=='}'){
+                    tempScope--;
+                }
+                if(tempScope==0){
+                    endOfComplex=i;
+                    break;
+                }
 
+            }
+            simpOrComp=new ComplexToken(input.substring(0,endOfComplex),scope+1,false);
 
         }else if(input.startsWith("for")){
+            int tempScope=0;
+            int endOfComplex=0;
+            for(int i=input.indexOf("{");i<input.length();i++){
+                if(input.charAt(i)=='{'){
+                    tempScope++;
+                }else if(input.charAt(i)=='}'){
+                    tempScope--;
+                }
+                if(tempScope==0){
+                    endOfComplex=i;
+                    break;
+                }
 
+            }
+            simpOrComp=new ComplexToken(input.substring(0,endOfComplex),scope+1,true);
         }
 
     }
