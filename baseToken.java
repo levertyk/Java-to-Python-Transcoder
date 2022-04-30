eab7c80bcdc184101caff1f980395abd1ecd264f
petermatern <58228250+petermatern@users.noreply.github.com>
2022-04-30 15:32:08 -0500

    Update baseToken.java
    updated base

---
+++ baseToken.java
@@ -0,0 +1,19 @@
+public class BaseToken extends Token {
+
+
+    Token simpOrComp;
+    BaseToken nextBase;
+    BaseToken(String input,int scope){
+        super(input, scope)
+
+        if(input.substring(0,6).contains("while")){
+
+        }
+        else if()
+
+    }
+        
+
+    }
+    
+}
