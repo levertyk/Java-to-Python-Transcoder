42aca90d399d286fee3f79d938709fc4e9e6e884
levertyk <levertyk4517@my.uwstout.edu>
2022-04-30 16:27:20 -0500

    Implemented SimpleToken

---
+++ SimpleToken.java
@@ -0,0 +1,17 @@
+public class SimpleToken extends Token{
+    
+    SimpleToken(String input, int scope) {
+        super(input, scope);
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        //  Remove the type if it exists (declarations)
+        //  Remove the semicolon at the end of the statement
+        output += input.replaceAll("int|String|double|boolean|char", "").replaceAll(";", "");
+
+        return output;
+    }
+}
