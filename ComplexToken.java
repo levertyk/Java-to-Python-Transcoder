b62c55b88e37210498cae4fb558b8f5366e2963a
levertyk <levertyk4517@my.uwstout.edu>
2022-05-01 15:34:10 -0500

    Removed forLoop parameter.

--- ComplexToken.java
+++ ComplexToken.java
@@ -2,9 +2,8 @@ public class ComplexToken extends Token {
     boolean isForLoop;
     BaseToken nextBase;
 
-    ComplexToken(String input, int scope, boolean isForLoop) {
+    ComplexToken(String input, int scope) {
         super(input, scope);
-        this.isForLoop = isForLoop;
         if (input.startsWith("while")) {
             nextBase = new BaseToken(input.substring(input.indexOf("{"), input.lastIndexOf("}")), scope + 1);
         }
