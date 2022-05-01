d5af44ce432ab60462ac36125e29e42c2eae73ea
levertyk <levertyk4517@my.uwstout.edu>
2022-05-01 15:33:27 -0500

    Encalsulation, formatting, documentation.

--- ClassToken.java
+++ ClassToken.java
@@ -1,3 +1,10 @@
+/**
+ * The class token will always start the parse tree, as it is assumed the java file will
+ * begin with a class. From here, a new base token is made with the inner code of the
+ * java class.
+ * 
+ * @author Keenan Leverty
+ */
 public class ClassToken extends Token {
 
     BaseToken middleToken;
@@ -5,11 +12,20 @@ public class ClassToken extends Token {
     ClassToken(String input) {
         super(input, 0);
 
-        BaseToken middleToken = new BaseToken(input.substring(input.indexOf('{') + 1, input.length() - 1), scope);
+        input = input.replaceAll("\\s|public|private|protected|static", "");
+
+        middleToken = new BaseToken(input.substring(input.indexOf('{') + 1, input.length() - 1), scope);
     }
 
     @Override
     public String toString() {
-        return middleToken.toString();
+        return generalChanges(middleToken.toString());
+    }
+
+    private String generalChanges(String in) {
+        String out = in.replaceAll("true", "True");
+        out = out.replaceAll("false", "False");
+
+        return out;
     }
 }
