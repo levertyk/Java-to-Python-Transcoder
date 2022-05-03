63882e216ff41eda9ece96071f25a577342b260c
levertyk <levertyk4517@my.uwstout.edu>
2022-05-02 21:15:50 -0500

    Made more complex testing file.

--- Test1.java
+++ Test1.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 public class Test1 {
-    int i = 0;
-    String variableName = "cornStarch";
-    double wow = 1.0;
-    boolean isBad = false;
-    char c = 'a';
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello world!");
+
+        int x = 0;
+        String variableName = "corn Starch";
+        String userInput = sc.next();
+        double wow = 1.0;
+        boolean isBad = false;
+        char c = 'a';
+
+        if(x==1) {
+            x+=1;
+        }
+
+        for (int i = 0; i < 10; i +=1) {
+            x = i;
+
+            if( i < 3) {
+                isBad = true;
+                System.out.println("Kill me, please!!!!!");
+            }
+        }
+
+        while(isBad) {
+            wow += 1;
+            isBad = false;
+        }
+    }
 }
