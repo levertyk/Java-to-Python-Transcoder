5436bad25b0d243e97c969d55cc6fcd44a239429
Keenan Leverty <91560030+levertyk@users.noreply.github.com>
2022-05-03 12:08:11 -0500

    fixed syntax

---
+++ testJavaCode.java
@@ -0,0 +1,59 @@
+import Java.util.Scanner;
+
+public class TestJavaCode.java
+    public static void main(String[] args){
+
+        int a = 8;
+        int b = 4;
+        int c = 0;
+        float d = 3.14;
+        float e = 6.85;
+        float f = 0.0;
+        String animal = "dog";
+        String statement = "is good";
+        char letter1 = 'c';
+        char letter2  = 'a';
+        char letter3 = 't';
+        boolean working = true;
+        String entry;
+        boolean holdRetuurn;
+
+        System.out.println(a + "this is var a");
+        System.out.println(b + "this is var b");
+        System.out.println(c + "this is var c");
+        System.out.println(a + b + "this is a + b");
+        c = a + b;
+        System.out.println(c + "this is a + b saved in c");
+        c = a - b;
+        System.out.println(c + "this is a - b saved in c");
+        c = a * b;
+        System.out.println(c + "this is a * b saved in c");
+        c = a / b;
+        System.out.println(c + "this is a / b saved in c");
+        c = a * (a - b);
+        System.out.println(c + "this is a * (a - b) saved in c");
+        f = d + e;
+        System.out.println(f + "this is d + e saved in c");
+        f = d * e;
+        System.out.println(f + "this is d * e saved in c");
+        System.out.println(animal);
+        System.out.println(statement);
+
+        System.out.println("enter a string");
+
+        if(working == true){
+            System.out.println(animal + statement);  
+        }
+
+        for(int i = 0; i < word.length(); i++){
+            Systerm.out.println(letter1 + letter2 + letter3)
+        }
+        if(holdRetuurn == true){
+            System.out.println("you have entered a string that contains letters that make up the word 'no' ");
+        }
+        else {
+            System.out.println("you have entered a string that does not contain all letters that make up the word 'no' ");
+        }
+
+    }
+}
