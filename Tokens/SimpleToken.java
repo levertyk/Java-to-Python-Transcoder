fb5a0ce6c005e900a21b5f23045961cb5242d535
levertyk <levertyk4517@my.uwstout.edu>
2022-05-02 21:20:11 -0500

    Implemented input and output simple tokens.

--- Tokens/SimpleToken.java
+++ Tokens/SimpleToken.java
@@ -14,6 +14,19 @@ class SimpleToken extends Token {
         // New output, filled at the beginning with the correct amount of tabs for the scope
         String output = gimmeTabs();
 
+        if(input.startsWith("System.out.println")) {
+            return output + "print(" + input.substring(input.indexOf("ln(\"") + 3, input.indexOf(");")) + ")\n";
+        }
+
+        if(input.startsWith("Scanner")) {
+            return "";
+        }
+
+        if(input.endsWith(".next();") || input.endsWith(".nextLine();")) {
+            String altOutput = output + input.replaceAll("=\\w*\\..*", "=") + "input()\n";
+            return altOutput.replaceAll("int|String|double|boolean|char", "").replaceAll(";", "");
+        }
+
         // Remove the type if it exists (declarations)
         // Remove the semicolon at the end of the statement
         output += input.replaceAll("int|String|double|boolean|char", "").replaceAll(";", "");
