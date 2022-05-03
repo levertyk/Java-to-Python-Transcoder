e8261b56d8e5a03e54b55e6dc95464ceec92437f
levertyk <levertyk4517@my.uwstout.edu>
2022-05-02 21:16:13 -0500

    Program generated output from Test1.java file.

---
+++ Test1.py
@@ -0,0 +1,20 @@
+
+print("Hello world!")
+x=0
+variableName="corn Starch"
+userInput=input()
+wow=1.0
+isBad=False
+c='a'
+if x==1 :
+	x+=1
+i=0
+while i<10 :
+	x=i
+	if i<3 :
+		isBad=True
+		print("Kill me, please!!!!!")
+	i+=1
+while isBad :
+	wow+=1
+	isBad=False
